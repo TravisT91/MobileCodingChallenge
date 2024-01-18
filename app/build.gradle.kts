28ce873a15d1782ffb2685db7961e674164f7292
Travis Tkachuk <travistkachuk@gmail.com>
2024-01-18 14:38:01 -0800

    Removed unused logging interceptor and reorganized dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,11 +56,10 @@ dependencies {
     val navVersion = "2.7.6"
     val lifeCycleVersion = "2.7.0"
     val retofitVersion = "2.9.0"
-    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
     implementation("com.google.android.gms:play-services-maps:18.2.0")
     implementation("com.squareup.retrofit2:retrofit:$retofitVersion")
     implementation("com.squareup.retrofit2:converter-gson:$retofitVersion")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
     implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
     implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
     implementation("androidx.activity:activity-ktx:$activityVersion")
